717feb16933448cebc9284bce8e6c4c74178d67f
Jilesh <jileshm.spaceo@gmail.com>
2022-08-17 15:29:34 +0530

    Initial Commit

---
+++ arch_practical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  arch_practical
+//
+//  Created by Jilesh Mahamunkar on 17/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
